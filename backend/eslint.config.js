9e2a09927035c31432149f2d9ac7d1d494a63245
mikokortelainen <mikokortelainen2003@gmail.com>
2025-10-30 16:04:51 +0200

    project skeleton, docker compose, gitlab ci for backend

---
+++ backend/eslint.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.stylistic,
+);
